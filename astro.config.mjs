478015ab156dae26de9eed925cc07d3057c3a096
Edvin Linden <edvin.linden@fortnox.se>
2022-09-04 09:55:15 +0200

    Update gh pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
-
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +10,5 @@ export default defineConfig({
       config: { path: "./tailwind.config.js" },
     }),
     sitemap(),
-    react(),
-    image(),
   ],
 });
