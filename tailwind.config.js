018764d6f8a4ab981a5a21c09ef8ba8850427ab9
Edvin Linden <edvin.linden@fortnox.se>
2022-11-18 15:03:05 +0100

    Add h1 tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,12 +20,18 @@ module.exports = {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
+        "button-pulse": {
+          "0%": { "box-shadow": "0px 0px 0px 0px rgba(37,99,235,0.7)" },
+          "50%": { "box-shadow": "0px 0px 0px 20px rgba(37,99,235,0)" },
+          "100%": { "box-shadow": "0px 0px 0px 20px rgba(37,99,235,0)" },
+        },
       },
       animation: {
         "hero-1": "hero-1 700ms ease-in-out",
         "hero-2": "hero-2 700ms ease-in-out",
         "hero-fade-in": "hero-fade-in 750ms ease-in-out",
         "fade-in": "fade-in 750ms ease-in-out forwards",
+        "button-pulse": "button-pulse 4000ms ease-in infinite",
       },
       backgroundImage: {
         "topo-pattern": "url('/images/topo-pattern.svg')",
