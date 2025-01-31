045711a6e44494249c57741d48ad8c109173dbbd
Edvin Linden <edvin.linden@fortnox.se>
2025-01-31 13:41:14 +0100

    Upgrade Tailwind to version 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://boxtimer.app/",
-  integrations: [
-    tailwind({
-      config: { path: "./tailwind.config.js" },
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
